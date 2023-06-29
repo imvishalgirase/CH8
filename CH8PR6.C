fdff2ee9ac1f8254396adb8d6c3126ef6efa5154
imvishalgirase <137286597+imvishalgirase@users.noreply.github.com>
2023-06-29 09:58:49 +0530

    Add files via upload

---
+++ CH8PR6.C
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int a,b,c,d;
+   clrscr();
+   printf("plz enter the value of A=");
+   scanf("%d",&a);
+    printf("plz enter the value of B=");
+    scanf("%d",&b);
+     printf("plz enter the value of C=");
+     scanf("%d",&c);
+
+      printf("plz enter the value of D=");
+
+      scanf("%d",&d);
+
+
+      (a>b)?(a>c)?(a>d)?printf("A is max "):printf("D is max"):(c>d)?printf("C is max"):printf("D is max ")
+
+		       :(b>c)?(b>d)?printf("B is max"):printf("D is max") :(c>d)?printf("C is max"):printf("D is max ") ;
+		       getch();
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
